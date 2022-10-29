196360e6188487886d95e42a0c6e995936d0d232
leandroleiteh <leandroluz2016162gmail.com>
2022-10-29 14:10:29 -0300

    Feito os estudos de Classes e metodos

--- CONHECENDO LINGUAGEM JAVA/Exercitando/src/aplicando_metodos_atributos_classes/Carro.java
+++ CONHECENDO LINGUAGEM JAVA/Exercitando/src/aplicando_metodos_atributos_classes/Carro.java
@@ -2,18 +2,40 @@ package aplicando_metodos_atributos_classes;
 
 public class Carro {
 
-	String nomeCarro;
-	String marca;
+	String modelo;
+	boolean alarme;
+	int velocidade;
 	String motorista;
-	boolean ligado = false;
-	int vel;
-
-	public void acelerar(int aceleracao) {
-		vel += aceleracao;
-
+	
+	public void marca(String qualMarca) {
+		modelo = qualMarca;
+		System.out.println("A marca do carro é: " + qualMarca);
 	}
 	
-	public void freiar(int reduzir) {
-		vel -= reduzir;
+	public void travado() {
+		alarme = true;
+		System.out.println("O alarme está travado: " + alarme);
+	}
+	
+	public void destravado() {
+		alarme = false;
+		System.out.println("O alarme está destravado: " + alarme);
+	}
+	
+	public void acelerar(int qntKmh) {
+		velocidade += qntKmh;
+		System.out.println("Acelerou " + qntKmh  + "Km/h");
+	}
+	
+	public void reduzir(int qntkmh) {
+		velocidade -= qntkmh;
+		System.out.println("Reduziu " + qntkmh + "Km/h");
+	}
+	
+	public void condutor(String quemDirige) {
+		motorista = quemDirige;
+		System.out.println("O motorista é: " + quemDirige);
 	}
 }
+
+
--- CONHECENDO LINGUAGEM JAVA/Exercitando/src/aplicando_metodos_atributos_classes/Dirigir.java
+++ CONHECENDO LINGUAGEM JAVA/Exercitando/src/aplicando_metodos_atributos_classes/Dirigir.java
@@ -4,25 +4,30 @@ public class Dirigir {
 
 	public static void main(String[] args) {
 
-		Carro c1 = new Carro();
-
-		c1.marca = "Ford";
-		c1.nomeCarro = "Ka";
-		c1.motorista = "Jussiara";
-		c1.vel = 50;
-
-		System.out.println("A marca do carro é: " + c1.marca);
-		System.out.println("O modelo é: " + c1.nomeCarro);
-		System.out.println("A motorista é: " + c1.motorista);
-
-		c1.acelerar(10);
-
-		System.out.println(c1.vel);
-
-		c1.freiar(30);
-
-		System.out.println(c1.vel);
-
+		Carro c2 = new Carro();
+		
+		c2.velocidade = 50;
+		
+		c2.travado();
+		
+		c2.destravado();
+		
+		c2.condutor("Leandro");
+		
+		c2.marca("Ford");
+		
+		System.out.println("Qual à velocidade atual do carro? " + c2.velocidade + "Km/h");
+		
+		c2.acelerar(10);
+		c2.acelerar(5);
+		
+		System.out.println("O carro esta à: " + c2.velocidade  + "Km/h");
+		
+		c2.reduzir(25);
+		
+		System.out.println("O carro esta à: " + c2.velocidade  + "Km/h");
+		
+		
 	}
 
 }
